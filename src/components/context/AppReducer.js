892cc6b5c300e885c726fa0b38edf885f164f7af
ellameta <74216472+ellameta@users.noreply.github.com>
2021-08-08 15:40:19 +0200

    Update AppReducer.js

--- src/components/context/AppReducer.js
+++ src/components/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+function AppReducer (state, action) {
     switch(action.type) {
         case "ADD_MOVIE_TO_FAVORITES":
             return {
@@ -14,3 +14,5 @@ export default (state, action) => {
         return state;
     }
 }
+
+export default AppReducer
